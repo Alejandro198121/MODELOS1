4a07ecd3f49d68b00e414b747c1e0fc452b01d59
Alejandro198121 <143757720+Alejandro198121@users.noreply.github.com>
2024-10-07 21:32:05 -0500

    Delete AbstractFactory directory

--- AbstractFactory/Ejemplo2AbstractFactory/src/model/fabricaAbstracta/Vehiculo.java
+++
@@ -1,5 +0,0 @@
-package model.fabricaAbstracta;
-
-public interface Vehiculo {
-    String mostrarDetalles();
-}
--- AbstractFactory/Ejemplo3AbstractFactory/src/model/fabricaAbstracta/Animal.java
+++
@@ -1,5 +0,0 @@
-package model.fabricaAbstracta;
-
-public interface Animal {
-    String emitirSonido();
-}
